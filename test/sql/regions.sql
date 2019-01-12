416c6b7eb6a08f23fcbce26329d5e804790e3431
Zacharias Knudsen <zachasme@gmail.com>
2019-01-12 14:46:15 +0100

    EHH: Improve region tests

--- test/sql/regions.sql
+++ test/sql/regions.sql
@@ -1,25 +1,31 @@
--- basecell around middle
-\set idx_basecell '\'8059fffffffffff\''
--- 7 child hexes in basecell
-\set idz_7hex 'ARRAY(SELECT h3_h3_to_children(:idx_basecell, 1))'
--- 6 child hexes in outer rim of basecell
-\set idz_6hex 'ARRAY(SELECT id FROM (SELECT h3_h3_to_children(:idx_basecell, 1) id) q WHERE id != \'81583ffffffffff\')'
+\pset tuples_only on
+-- res 0 index
+\set res0index '\'8059fffffffffff\''
+-- center hex
+\set center '\'81583ffffffffff\''
+-- 7 child hexes in res 0 index
+\set solid 'ARRAY(SELECT h3_h3_to_children(:res0index, 1))'
+-- 6 child hexes in rim of res 0 index
+\set hollow 'array_remove(:solid, :center)'
 
-SELECT h3_h3_get_resolution(:idx_basecell);
+--
+-- TEST h3_polyfill and h3_h3_set_to_linked_geo
+--
 
--- BACK AND FORTH
-SELECT :idz_7hex;
-SELECT (h3_h3_set_to_linked_geo(:idz_7hex)).exterior, (h3_h3_set_to_linked_geo(:idz_7hex)).holes;
-SELECT h3_polyfill(exterior, holes, 1) FROM 
-(
- SELECT *  FROM h3_h3_set_to_linked_geo(:idz_7hex)
+-- h3_polyfill is inverse of h3_h3_set_to_linked_geo for set without holes
+SELECT array_agg(result) is null FROM (
+    SELECT h3_polyfill(exterior, holes, 1) result FROM (
+        SELECT exterior, holes FROM h3_h3_set_to_linked_geo(:solid)
+    ) qq
+    EXCEPT SELECT unnest(:solid) result
 ) q;
 
-SELECT :idz_6hex;
-SELECT (h3_h3_set_to_linked_geo(:idz_6hex)).exterior, (h3_h3_set_to_linked_geo(:idz_6hex)).holes;
-SELECT h3_polyfill(exterior, holes, 1) FROM 
-(
- SELECT *  FROM h3_h3_set_to_linked_geo(:idz_6hex)
+-- h3_polyfill is inverse of h3_h3_set_to_linked_geo for set with a hole
+SELECT array_agg(result) is null FROM (
+    SELECT h3_polyfill(exterior, holes, 1) result FROM (
+        SELECT exterior, holes FROM h3_h3_set_to_linked_geo(:hollow)
+    ) qq
+    EXCEPT SELECT unnest(:hollow) result
 ) q;
 
 
