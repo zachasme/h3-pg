fc15d375969b2dacca666899410a27c81b9cbc57
Zacharias Knudsen <zachasme@gmail.com>
2019-01-12 14:24:30 +0100

    ENH: Improve h3-pg tests

--- test/sql/h3-pg.sql
+++ test/sql/h3-pg.sql
@@ -1,2 +1,19 @@
-SELECT h3_haversine_distance('8f2830828052d25', '8f283082a30e623');
-SELECT h3_basecells();
+\pset tuples_only on
+
+-- neighbouring indexes (one hexagon, one pentagon) at resolution 3
+\set hexagon '\'831c02fffffffff\'::h3index'
+\set pentagon '\'831c00fffffffff\'::h3index'
+\set distance 86.3352698485577
+\set epsilon 0.0000000000001
+
+--
+-- TEST h3_haversine_distance
+--
+
+SELECT h3_haversine_distance(:hexagon, :pentagon) - :distance < :epsilon;
+
+--
+-- TEST h3_basecells
+--
+
+SELECT COUNT(*) = 122 FROM (SELECT h3_basecells()) q;
