668d5d9ba8b12ca7192261c75f2bb024b0adf7a0
Zacharias Knudsen <zachasme@gmail.com>
2019-10-15 11:59:12 +0200

    Add tests for contains and contained operators

--- test/sql/type.sql
+++ test/sql/type.sql
@@ -11,6 +11,10 @@ SELECT :hexagon = :hexagon;
 SELECT NOT :hexagon = :pentagon;
 SELECT NOT :hexagon <> :hexagon;
 SELECT :hexagon <> :pentagon;
+SELECT :pentagon <@ h3_to_parent(:pentagon);
+SELECT bool_and(:pentagon @> c) FROM (
+    SELECT h3_to_children(:pentagon) c
+) q;
 
 --
 -- TEST bigint casting
