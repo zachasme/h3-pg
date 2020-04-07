5cd50e5430bfefacc93a4b76a60175e562a90e2d
Zacharias Knudsen <zachasme@gmail.com>
2020-04-07 16:16:32 +0200

    WIP CI

---
+++ .github/docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+set -e
+
+DISTRIBUTION=$1
+
+service postgresql start
+chmod -R a+w test
+
+if [ -z "$DISTRIBUTION" ]; then
+    make install
+    su - postgres -p -c "make ci"
+    su - postgres -p -c "make installcheck"
+else
+    pgxn install $DISTRIBUTION
+    su - postgres -p -c "pgxn load $DISTRIBUTION"
+    su - postgres -p -c "pgxn check $DISTRIBUTION"
+fi
