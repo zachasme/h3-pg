c18f632111779e1e0275e6a81ed15108976ec09a
Zacharias Knudsen <z@chari.as>
2022-04-12 13:37:21 +0200

    Remove debugging from Dockerfile entrypoint

--- .github/docker/entrypoint.sh
+++ .github/docker/entrypoint.sh
@@ -8,12 +8,10 @@ chmod -R a+w h3/test
 
 if [ -z "$DISTRIBUTION" ]; then
     make install
-    su postgres -p -c "ls"
-    su postgres -p -c "pwd"
     su postgres -p -c "make ci"
     su postgres -p -c "make installcheck"
 else
     pgxn install $DISTRIBUTION
-    su - postgres -p -c "pgxn load $DISTRIBUTION"
-    su - postgres -p -c "pgxn check $DISTRIBUTION"
+    su postgres -p -c "pgxn load $DISTRIBUTION"
+    su postgres -p -c "pgxn check $DISTRIBUTION"
 fi
